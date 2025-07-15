0fde6bec5dc2a5c42ab3e4280659b82d07f61d6a
Chris-Cameron <chriscameront@gmail.com>
2025-07-15 15:01:38 -0400

    Functioning scale & intensity options in noise generator

--- src/simplex.ts
+++ src/simplex.ts
@@ -11,12 +11,15 @@ function addRow(): void{
 
     let intensity = newRow.insertCell();
     intensity.contentEditable = "true";
+    intensity.textContent = "100";
 
     let scale = newRow.insertCell();
     scale.contentEditable = "true";
+    scale.textContent = "100";
 
     let exponent = newRow.insertCell();
     exponent.contentEditable = "true";
+    exponent.textContent = "1";
 }
 
 let rowButton = document.getElementById("addRowButton") as HTMLButtonElement;
@@ -24,17 +27,24 @@ rowButton.addEventListener("click", addRow);
 
 
 function drawNoise(): void{
+    let table = document.getElementById("octaveTable") as HTMLTableElement;
     let noise2D = createNoise2D();
     let canvas = document.getElementById("resultWindow") as HTMLCanvasElement;
     let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    let scale = .01;
-    for(let y = 0; y < canvas.clientHeight; y++){
-        for(let x = 0; x < canvas.clientWidth; x++){
-            let brightness = (noise2D(scale*x,scale*y)+1)*127.5;
+
+   for(let x = 0; x < canvas.clientWidth; x++){
+        for(let y = 0; y < canvas.clientHeight; y++){
+            let brightness = 0;
+            for(let i = 1; i < table.rows.length; i++){
+                let row = table.rows[i];
+                let scale = Number(row.cells[2].textContent);
+                let intensity = Number(row.cells[1].textContent);
+                brightness += (noise2D(x/scale,y/scale)+1)*intensity*1.275;
+            } 
             ctx.fillStyle = `rgb(${brightness},${brightness},${brightness} )`
             ctx.fillRect(x, y, 1, 1)
         }
-    }
+   }
 }
 
 let drawButton = document.getElementById("drawNoiseButton") as HTMLButtonElement;
